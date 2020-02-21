9556574287c75e23a840bf927031a07b3349dda3
milindar <milindarreddy.1996@gmail.com>
2020-02-21 16:57:57 -0600

    added data.txt file

--- data/count-of-segment/mapper.py
+++ data/count-of-segment/mapper.py
@@ -1,10 +1,10 @@
-input = open("purchases.txt", "r")
+input = open("data.txt", "r")
 output = open("01.txt", "w")
 
 for line in input:
-    datalist = line.strip().split("    ")
-    date, time,store, item, cost, paymentType = datalist
-    output.write(paymentType + "\t" + cost + "\n")
+    datalist = line.strip().split("\t")
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u = datalist
+    output.write(o + "\t" + r + "\n")
 
 input.close()
 output.close()
