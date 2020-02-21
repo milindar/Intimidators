33346e08c92db57a3e3d1bb670d50fc15c2b41df
pradeepkumar <pradeepkumartheegala@gmail.com>
2020-02-21 16:13:48 -0600

    Commit

--- data/maximum-no-of-sales/mapper.py
+++ data/maximum-no-of-sales/mapper.py
@@ -1,10 +1,10 @@
-input = open("purchases.txt", "r")
+input = open("data.txt", "r")
 output = open("01.txt", "w")
 
 for line in input:
     datalist = line.strip().split("    ")
     a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u = datalist
-    output.write(paymentType + "\t" + cost + "\n")
+    output.write(o + "\t" + r + "\n")
 
 input.close()
 output.close()
