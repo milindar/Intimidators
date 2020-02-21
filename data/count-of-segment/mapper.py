e888a3050c54e316d80183cbce6acd70190323ef
milindar <milindarreddy.1996@gmail.com>
2020-02-21 17:06:02 -0600

    added reducer code

--- data/count-of-segment/mapper.py
+++ data/count-of-segment/mapper.py
@@ -4,7 +4,7 @@ output = open("01.txt", "w")
 for line in input:
     datalist = line.strip().split("\t")
     a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u = datalist
-    output.write(o + "\t" + r + "\n")
+    output.write(h + "\t" + r + "\n")
 
 input.close()
 output.close()
---
+++ data/count-of-segment/reducer.py
@@ -0,0 +1,28 @@
+s = open("01.txt","r")
+r = open("final.txt", "w")
+
+thisKey = ""
+thisValue = 0.0
+
+for line in s:
+  data = line.strip().split('\t')
+  segment, sales = data
+
+  if segment != thisKey:
+    if thisKey:
+      # output the last key value pair result
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+    # start over when changing keys
+    thisKey = segment 
+    thisValue = 0.0
+  
+  # apply the aggregation function
+
+  thisValue += 1
+
+# output the final entry when done
+r.write(thisKey + '\t' + str(thisValue)+'\n')
+
+s.close()
+r.close()
