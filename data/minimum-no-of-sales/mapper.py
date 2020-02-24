8b3455c5cb0dff36c566fa0bb56177dfd39be58c
sunilmundru <46757390+sunilmundru@users.noreply.github.com>
2020-02-24 14:34:14 -0600

    commit

--- data/minimum-no-of-sales/mapper.py
+++ data/minimum-no-of-sales/mapper.py
@@ -1,10 +1,17 @@
+# Reading the data from text file
 input = open("data.txt", "r")
+#Storing the Mapper output in 01.txt file
 output = open("01.txt", "w")
 
+# iterating and getting each line
 for line in input:
+    # dividing the data using tab seperator
     datalist = line.strip().split("\t")
+    # storing the values in variables
     a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u = datalist
+    # writing values to output file
     output.write(o + "\t" + r + "\n")
 
+# closing the connection
 input.close()
 output.close()
